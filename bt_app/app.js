797943ce0dbb2842bbe0d645d779744af87547be
A.R <thecult899@protonmail.com>
2020-05-15 00:08:42 +0200

    usage of http,https,301 http->https for prod or dev (npm run prod/npm run dev)

--- bt_app/app.js
+++ bt_app/app.js
@@ -20,138 +20,181 @@ const requestIp = require('request-ip')
 const helmet = require('helmet')
 const expectCt = require('expect-ct')
 const featurePolicy = require('feature-policy')
+const argv = require('yargs').argv
 
 
 var debug = Debug('App.js');
+
 debug = console.log.bind(console);
 
-var app = express();
-var httpApp = express();
+var httpsApp;
+var httpApp;
+var apps = [];
 
-app.use(helmet())
-app.use(expectCt({
-  enforce: true,
-  maxAge: 86400
-}))
-app.use(featurePolicy({
-  features: {
-    fullscreen: ["'self'"]
-  }
-}))
-
-app.disable('x-powered-by')
-httpApp.disable('x-powered-by')
-
-httpApp.set('port', process.env.PORT || 80);
-httpApp.use(express.static(path.join(__dirname, 'public')));
-httpApp.use(compression());
-httpApp.all("*", function (req, res, next) {
-  if(req.url === "/sitemap.xml"){
-    req.pipe(filed('./public/sitemap.xml')).pipe(resp);
-  }
-  else {
-    var hn = req.headers.host;
-    if (hn.match(/^www/) !== null) {
-      hn = hn.replace(/^www\./, '')
+if (argv['redirect-http']) {
+  argv.http = true;
+  httpApp = express();
+  console.log('redirect 301 http->https')
+  httpApp.all("*", function (req, res, next) {
+    if(req.url === "/sitemap.xml"){
+      req.pipe(filed('./public/sitemap.xml')).pipe(resp);
     }
-    res.redirect(301, "https://" + hn + "/");
-  }
-});
-// view engine setup
-debug('setting views');
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+    else {
+      var hn = req.headers.host;
+      if (hn.match(/^www/) !== null) {
+        hn = hn.replace(/^www\./, '')
+      }
+      res.redirect(301, "https://" + hn + "/");
+    }
+  });
+}
 
-debug('declaring middlewares');
-app.use(requestIp.mw())
-app.use(function(req, res, next) {
-  const ip = req.clientIp;
-  debug(`client ip: ${ip}`);
-  next();
-})
-app.use(csp({
-  // Specify directives as normal.
-  directives: {
-    defaultSrc: ["'none'"],
-    connectSrc: ["'self'"],
-    mediaSrc: ["'self'"],
-    scriptSrc: ["'self'", /*"'strict-dynamic'",*/ "'unsafe-inline'", 'https://www.googletagmanager.com', 'www.google-analytics.com', 'code.jquery.com', 'cdnjs.cloudflare.com', 'cdn.jsdelivr.net'],
-    styleSrc: ["'self'"],
-    fontSrc: ["'self'"],
-    baseUri: ["'self'"],
-    imgSrc: ["'self'", 'www.google-analytics.com'],
-    reportUri: '/report-violation',
-    objectSrc: ["'none'"],
-    frameAncestors: ["'none'"],
-    upgradeInsecureRequests: true,
-    workerSrc: false  // This is not set.
-  },
+if(argv.http){
+  console.log('creating http app')
+  if (!httpApp)
+    httpApp = express();
+  httpApp.set('port', process.env.HTTP_PORT || 80);
+  apps.push(httpApp);
+}
 
-  // This module will detect common mistakes in your directives and throw errors
-  // if it finds any. To disable this, enable "loose mode".
-  loose: false,
+if(argv.https){
+  console.log('creating https app')
+  httpsApp = express();
+  var app = httpsApp;
+  app.set('port', process.env.HTTPS_PORT || 443);
 
-  // Set to true if you only want browsers to report errors, not block them.
-  // You may also set this to a function(req, res) in order to decide dynamically
-  // whether to use reportOnly mode, e.g., to allow for a dynamic kill switch.
-  reportOnly: false,
+  app.use(helmet())
+  app.use(expectCt({
+    enforce: true,
+    maxAge: 86400
+  }))
+  app.use(featurePolicy({
+    features: {
+      fullscreen: ["'self'"]
+    }
+  }))
+  app.disable('x-powered-by')
+ 
+  app.use(csp({
+    // Specify directives as normal.
+    directives: {
+      defaultSrc: ["'none'"],
+      connectSrc: ["'self'"],
+      mediaSrc: ["'self'"],
+      scriptSrc: ["'self'", /*"'strict-dynamic'",*/ "'unsafe-inline'", 'https://www.googletagmanager.com', 'www.google-analytics.com', 'code.jquery.com', 'cdnjs.cloudflare.com', 'cdn.jsdelivr.net'],
+      styleSrc: ["'self'"],
+      fontSrc: ["'self'"],
+      baseUri: ["'self'"],
+      imgSrc: ["'self'", 'www.google-analytics.com'],
+      reportUri: '/report-violation',
+      objectSrc: ["'none'"],
+      frameAncestors: ["'none'"],
+      upgradeInsecureRequests: true,
+      workerSrc: false  // This is not set.
+    },
 
-  // Set to true if you want to blindly set all headers: Content-Security-Policy,
-  // X-WebKit-CSP, and X-Content-Security-Policy.
-  setAllHeaders: false,
+    // This module will detect common mistakes in your directives and throw errors
+    // if it finds any. To disable this, enable "loose mode".
+    loose: false,
 
-  // Set to true if you want to disable CSP on Android where it can be buggy.
-  disableAndroid: false,
+    // Set to true if you only want browsers to report errors, not block them.
+    // You may also set this to a function(req, res) in order to decide dynamically
+    // whether to use reportOnly mode, e.g., to allow for a dynamic kill switch.
+    reportOnly: false,
 
-  // Set to false if you want to completely disable any user-agent sniffing.
-  // This may make the headers less compatible but it will be much faster.
-  // This defaults to `true`.
-  browserSniff: true
-}))
-app.use(dontSniffMimetype())
-app.use(compression());
-app.use(hsts({maxAge: 31536000, includeSubDomains: true, preload: true}))
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(cookieParser());
+    // Set to true if you want to blindly set all headers: Content-Security-Policy,
+    // X-WebKit-CSP, and X-Content-Security-Policy.
+    setAllHeaders: false,
 
-debug('Init routes');
-app.all('/*', function(req, res, next) {
-  debug(req.headers.host);
-  if (req.headers.host.match(/^www/) !== null ) {
-    res.redirect(301, 'https://' + req.headers.host.replace(/^www\./, '') + req.url);
-  } else {
+    // Set to true if you want to disable CSP on Android where it can be buggy.
+    disableAndroid: false,
+
+    // Set to false if you want to completely disable any user-agent sniffing.
+    // This may make the headers less compatible but it will be much faster.
+    // This defaults to `true`.
+    browserSniff: true
+  }))
+  app.use(dontSniffMimetype())
+  app.use(compression());
+  app.use(hsts({maxAge: 31536000, includeSubDomains: true, preload: true}))
+
+  app.all('/*', function(req, res, next) {
+    debug(req.headers.host);
+    if (req.headers.host.match(/^www/) !== null ) {
+      res.redirect(301, 'https://' + req.headers.host.replace(/^www\./, '') + req.url);
+    } else {
+      next();
+    }
+  })
+
+  apps.push(app);
+}
+
+for (app of apps){
+
+  if (app.get('port') == 80 && argv['redirect-http']) 
+    console.log('passing http routing (301 rediretion)');
+
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'pug');
+  app.use(requestIp.mw())
+  app.use(function(req, res, next) {
+    const ip = req.clientIp;
+    debug(`client ip: ${ip}`);
     next();
-  }
-})
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/submitPseudo', pseudoRouter);
-app.use('/session', sessionRouter);
+  })
+  app.use(logger('dev'));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(cookieParser());
 
-// start a test game session
-debug('Creating a game Session');
-sessionTest = new Session();
-sessions.push(sessionTest);
+  debug('Init routes');
+  app.use('/', indexRouter);
+  app.use('/users', usersRouter);
+  app.use('/submitPseudo', pseudoRouter);
+  app.use('/session', sessionRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  debug(`remote ip: ${req.ip}`)
-  next(createError(404));
-});
+  // start a test game session
+  debug('Creating a game Session');
+  sessionTest = new Session();
+  sessions.push(sessionTest);
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // catch 404 and forward to error handler
+  app.use(function(req, res, next) {
+    debug(`remote ip: ${req.ip}`)
+    next(createError(404));
+  });
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+  // error handler
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-module.exports = {app, httpApp};
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+/*
+  httpApp.use(compression());
+  httpApp.all("*", function (req, res, next) {
+    if(req.url === "/sitemap.xml"){
+      req.pipe(filed('./public/sitemap.xml')).pipe(resp);
+    }
+    else {
+      var hn = req.headers.host;
+      if (hn.match(/^www/) !== null) {
+        hn = hn.replace(/^www\./, '')
+      }
+      res.redirect(301, "https://" + hn + "/");
+    }
+  });
+  // view engine setup
+  debug('setting views');
+
+  debug('declaring middlewares');
+  */
+}
+
+module.exports = {httpsApp, httpApp, argv};
